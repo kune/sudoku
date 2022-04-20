67bec6e6193f20ec25e1550a59e94a5659b4d918
Alexander Kune <alexander@kune.de>
2022-04-20 16:38:19 +0200

    feat: find all solutions instead of only one

--- src/main/java/de/kune/sudoku/Sudoku.java
+++ src/main/java/de/kune/sudoku/Sudoku.java
@@ -1,5 +1,9 @@
 package de.kune.sudoku;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Sudoku {
 
     private final byte[][] grid;
@@ -55,42 +59,58 @@ public class Sudoku {
         return true;
     }
 
-    public Sudoku withValue(int column, int row, byte value) {
+    private void setValue(int column, int row, byte value) {
         if (row < 0 || row >= grid.length || column < 0 || column > grid[0].length) {
             throw new IllegalArgumentException("Column and row must be between 1 and 8");
         }
         if (value < 0 || value > 9) {
             throw new IllegalArgumentException("Value must be between 0 and 9");
         }
+        grid[row][column] = value;
+    }
+
+    public Sudoku withValue(int column, int row, byte value) {
         Sudoku result = new Sudoku(this.grid);
-        result.grid[row][column] = value;
+        result.setValue(column, row, value);
         return result;
     }
 
-    public Sudoku solve() {
+    public List<Sudoku> solve() {
         for (int row = 0; row < grid.length; row++) {
             for (int column = 0; column < grid[row].length; column++) {
                 if (isEmpty(column, row)) {
+                    List<Sudoku> candidates = new ArrayList<>();
                     for (byte v = 1; v < 10; v++) {
                         if (isValidValue(column, row, v)) {
                             Sudoku cand = withValue(column, row, v);
-                            if (row < grid.length - 1 || column < grid[row].length - 1) {
-                                cand = cand.solve();
-                            }
-                            if (cand != null) {
-                                return cand;
+                            if (cand.isSolved()) {
+                                candidates.add(cand);
+                            } else {
+                                candidates.addAll(cand.solve());
                             }
                         }
                     }
-                    return null;
+                    return candidates;
                 }
             }
         }
-        return null;
+        return Collections.emptyList();
+    }
+
+    public boolean isSolved() {
+        for (byte[] row: grid) {
+            for (byte cell: row) {
+                if (cell == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public String toString() {
-        StringBuilder result = new StringBuilder(100);
+        StringBuilder result = new StringBuilder(120);
+        result.append("\n");
         for (int row = 0; row < grid.length; row++) {
             for (int column = 0; column < grid[row].length; column++) {
                 result.append(grid[row][column]);
@@ -99,7 +119,7 @@ public class Sudoku {
                 }
             }
             result.append("\n");
-            if ((row+1) % 3 == 0) {
+            if ((row+1) % 3== 0 && row < 8 ) {
                 result.append("\n");
             }
         }
@@ -107,32 +127,32 @@ public class Sudoku {
     }
 
     public static void main(String[] args) {
-//        Sudoku sudoku = new Sudoku(new byte[][] {
-//                {0,6,4, 3,5,0, 2,0,1,},
-//                {3,0,0, 2,0,0, 6,0,0,},
-//                {0,9,7, 1,0,4, 3,5,0,},
-//
-//                {1,3,2, 4,0,0, 9,6,0,},
-//                {0,0,9, 0,1,0, 8,2,4,},
-//                {4,8,6, 9,0,0, 0,3,0,},
-//
-//                {7,0,0, 0,0,6, 0,1,3,},
-//                {0,0,3, 8,4,0, 7,9,2,},
-//                {9,4,1, 7,0,0, 0,8,0,},
-//        });
         Sudoku sudoku = new Sudoku(new byte[][] {
-                {1,0,0, 0,0,0, 0,0,0,},
-                {0,0,0, 0,0,0, 0,0,0,},
-                {0,0,0, 0,0,0, 0,0,0,},
+                {0,6,4, 3,5,0, 2,0,1,},
+                {3,0,0, 2,0,0, 6,0,0,},
+                {0,9,7, 1,0,4, 3,5,0,},
 
-                {0,0,0, 0,0,0, 0,0,0,},
-                {0,0,0, 0,0,0, 0,0,0,},
-                {0,0,0, 0,0,0, 0,0,0,},
+                {1,3,2, 4,0,0, 9,6,0,},
+                {0,0,9, 0,1,0, 8,2,4,},
+                {4,8,6, 9,0,0, 0,3,0,},
 
-                {0,0,0, 0,0,0, 0,0,0,},
-                {0,0,0, 0,0,0, 0,0,0,},
-                {0,0,0, 0,0,0, 0,0,0,},
+                {7,0,0, 0,0,6, 0,1,3,},
+                {0,0,3, 8,4,0, 7,9,2,},
+                {9,4,1, 7,0,0, 0,8,0,},
         });
+//        Sudoku sudoku = new Sudoku(new byte[][] {
+//                {1,0,0, 0,0,0, 0,0,0,},
+//                {0,0,0, 0,0,0, 0,0,0,},
+//                {0,0,0, 0,0,0, 0,0,0,},
+//
+//                {0,0,0, 0,0,0, 0,0,0,},
+//                {0,0,0, 0,0,0, 0,0,0,},
+//                {0,0,0, 0,0,0, 0,0,0,},
+//
+//                {0,0,0, 0,0,0, 0,0,0,},
+//                {0,0,0, 0,0,0, 0,0,0,},
+//                {0,0,0, 0,0,0, 0,0,0,},
+//        });
         System.out.println(sudoku);
         System.out.println(sudoku.solve());
 
